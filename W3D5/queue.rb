6f5ecd6804d2c7e6c07168140279501265fb479b
Susan Zea <susanzea16@gmail.com>
2021-10-21 21:18:36 -0400

    completed queue class

---
+++ W3D5/queue.rb
@@ -0,0 +1,20 @@
+require_relative "./stack.rb"
+
+class Queue
+
+    def initialize
+        @queue = []
+    end
+
+    def enqueue(el)
+        @queue.push(el)
+    end
+
+    def dequeue
+        @queue.shift
+    end
+
+    def peek
+        @queue[0]
+    end
+end
