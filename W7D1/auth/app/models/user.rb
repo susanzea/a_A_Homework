26fb44c1fdc4efee517a03c0f163ffd888cae5b9
Susan Zea <susanzea16@gmail.com>
2021-11-14 18:26:02 -0500

    need to ask about issue with password= method

---
+++ W7D1/auth/app/models/user.rb
@@ -0,0 +1,41 @@
+class User < ApplicationRecord
+    attr_accessor :password
+    validates :username, :session_token, presence: true
+    validates_presence_of :password_digest, message: "Password can't be blank"
+    validates :password, length: { minimum: 6, allow_nil: true }
+
+    before_validation :ensure_session_token
+
+
+    def self.find_by_credentials(username, password)
+        user = User.find_by(username: username)
+        return nil if user.nil?
+        user.is_password?(password) ? user : nil
+    end
+
+    def self.generate_session_token
+        @session_token = SecureRandom::urlsafe_base64
+    end
+
+    def reset_session_token!
+        self.session_token = User.generate_session_token
+        self.save
+    end
+
+    def ensure_session_token 
+        self.session_token ||= User.generate_session_token
+    end
+
+    def password=(password)
+        debugger
+        @password = password
+        @password_digest = BCrypt::Password.create(password)
+    end
+
+    def is_password?(password)
+        Bcrypt::Password.new(@password_digest).is_password?(password)
+    end
+
+    
+
+end
