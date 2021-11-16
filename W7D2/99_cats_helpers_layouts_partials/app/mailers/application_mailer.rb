37da5eac84befd52a6bd70a5ec9df3aaf6075e50
Susan Zea <susanzea16@gmail.com>
2021-11-15 21:48:50 -0500

    completed W7D2 hw

--- W7D2/99_cats_helpers_layouts_partials/app/mailers/application_mailer.rb
+++ W7D2/99_cats_helpers_layouts_partials/app/mailers/application_mailer.rb
@@ -1,4 +1,11 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
+  default from: 'everybody@appacademy.io'
   layout 'mailer'
+
+
+    def welcome_email(user)
+        @user = user
+        @url  = 'http://example.com/login'
+        mail(to: @user.username, subject: 'Welcome to 99 Cats')
+    end
 end
---
+++ W7D2/99_cats_helpers_layouts_partials/app/mailers/user_mailer.rb
@@ -0,0 +1,5 @@
+class UserMailer < ApplicationMailer
+
+
+
+end
